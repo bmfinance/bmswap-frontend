5dd4758d784c0bb2af7a24e5916bbb1982c4efb2
matsuda nakamoto <bmfinancemail@gmail.com>
2021-11-19 09:33:20 +0800

    bnb->xdai

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -16,20 +16,12 @@ const config: MenuEntry[] = [
     },
     items: [
       {
-        label: 'LP Migration',
-        href: '/migrate',
-      },
-      {
         label: 'Exchange',
         href: 'https://bm.finance/#/swap',
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.bm.finance/#/pool',
-      },
-      {
-        label: 'V1 Liquidity (Old)',
-        href: '/pool',
+        href: 'https://bm.finance/#/pool',
       },
     ],
   },
@@ -44,99 +36,9 @@ const config: MenuEntry[] = [
     href: 'https://bm.finance/pools',
   },
   {
-    label: 'Prediction (BETA)',
-    icon: 'PredictionsIcon',
-    href: 'https://bm.finance/prediction',
-  },
-  {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: 'https://bm.finance/lottery',
-  },
-  {
-    label: 'Collectibles',
-    icon: 'NftIcon',
-    href: 'https://bm.finance/nft',
-  },
-  {
-    label: 'Team Battle',
-    icon: 'TeamBattleIcon',
-    href: 'https://bm.finance/competition',
-  },
-  {
-    label: 'Teams & Profile',
-    icon: 'GroupsIcon',
-    items: [
-      {
-        label: 'Leaderboard',
-        href: 'https://bm.finance/teams',
-      },
-      {
-        label: 'Task Center',
-        href: 'https://bm.finance/profile/tasks',
-      },
-      {
-        label: 'Your Profile',
-        href: 'https://bm.finance/profile',
-      },
-    ],
-  },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'Overview',
-        href: 'https://pancakeswap.info',
-      },
-      {
-        label: 'Tokens',
-        href: 'https://pancakeswap.info/tokens',
-      },
-      {
-        label: 'Pairs',
-        href: 'https://pancakeswap.info/pairs',
-      },
-      {
-        label: 'Accounts',
-        href: 'https://pancakeswap.info/accounts',
-      },
-    ],
-  },
-  {
     label: 'IFO',
     icon: 'IfoIcon',
-    href: 'https://bm.finance/ifo',
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Contact',
-        href: 'https://github.com/bmfinance',
-      },
-      {
-        label: 'Voting',
-        href: 'https://github.com/bmfinance',
-      },
-      {
-        label: 'Github',
-        href: 'https://github.com/bmfinance',
-      },
-      {
-        label: 'Docs',
-        href: 'https://github.com/bmfinance',
-      },
-      {
-        label: 'Blog',
-        href: 'https://github.com/bmfinance',
-      },
-      {
-        label: 'Merch',
-        href: 'https://github.com/bmfinance',
-      },
-    ],
+    href: '#',
   },
 ]
 
